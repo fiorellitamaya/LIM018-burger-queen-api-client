5ceb0fb352f9247096ca03b0cbf5168f60aacd9b
Denimarx <denimar2022@gmail.comm>
2022-10-19 21:21:18 -0500

    css /login

--- my-app/src/paginas/login/login.jsx
+++ my-app/src/paginas/login/login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import fotoBQ from '../../assets/imagenesBQ/fotoBQ.jpg'
+//import fotoBQ from '../../assets/imagenesBQ/fotoBQ.jpg'
+import loguitoBQ from '../../assets/imagenesBQ/loguitoBQ.png'
 import './login.css';
 
 
@@ -7,21 +8,26 @@ export default function login() {
     return (
         <div className="containerLoginPrincipal">
 
-            <div className="containerLoginForm">
 
-                <img src ={fotoBQ} className="fotoBQ" alt="fotoBQ" />
-
+          
+            <div className="container-form-loguito">
+            <img src={loguitoBQ} className="loguitoBQ" alt="loguitoBQ" />
+            <h1>Burger Queen</h1>
                 <div className="form-login">
-                  <br />
-                  <input type="email" placeholder="Correo Electrónico" className="form-input-login" />
-                  <br />
-                  <input type="password" placeholder="Contraseña" className="form-input-login" name="password" />
-                  <br />
-                  <button type="submit" className="btn-login">Ingresar</button>
+                
+                    <br />
+                    <input type="email" placeholder="Usuario" className="form-input-login" />
+                    <br />
+                    <input type="password" placeholder="Contraseña" className="form-input-login" name="password" />
+                    <br />
+                    <button type="submit" className="btn-login">Ingresar</button>
                 </div>
-
             </div>
-            
+
+
+
+
+
         </div>
     )
 }
