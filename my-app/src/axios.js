24ae5d52c503544cf7d8c995ae528cbf4737627f
fiorellitamaya <fiorellitamaya@hotmail.com>
2022-10-25 11:54:00 -0500

    Probando axios en pre-main

--- my-app/src/axios.js
+++ my-app/src/axios.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 const url = 'http://localhost:3001';
 const path = '/user'
 
-export function signIn(email, password){
-axios.post(`${url}${path}`, {email, password})
+export function signIn(username, password){
+axios.post(`${url}${path}`, {username, password})
 }
